cc44117ea843ed39a10e59c672b159ed84663264
Vaishnav Anil <vaishnav.nair@horizonind.org>
2023-11-05 17:41:48 +0530

    JwtValidator method name refactor

--- src/main/java/io/github/vshnv/nortviz/auth/jwt/JwtValidator.java
+++ src/main/java/io/github/vshnv/nortviz/auth/jwt/JwtValidator.java
@@ -4,5 +4,5 @@ import java.util.Optional;
 
 public interface JwtValidator {
     boolean validate(final String token);
-    Optional<String> getUidIfValid(final String token);
+    Optional<String> getUsernameIfValid(final String token);
 }

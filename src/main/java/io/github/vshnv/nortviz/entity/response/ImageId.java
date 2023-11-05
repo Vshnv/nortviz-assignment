0287af0cd7ba873624f0616330cc03ec37605855
Vaishnav Anil <vaishnav.nair@horizonind.org>
2023-11-06 02:09:51 +0530

    Added ImageId

---
+++ src/main/java/io/github/vshnv/nortviz/entity/response/ImageId.java
@@ -0,0 +1,13 @@
+package io.github.vshnv.nortviz.entity.response;
+
+public class ImageId {
+    private final String id;
+
+    public ImageId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}

640fd471c498efbace903bc363957c87bdb1dc37
Vaishnav Anil <vaishnav.nair@horizonind.org>
2023-11-06 02:04:42 +0530

    Implemented upload for ImageManagementController

---
+++ src/main/java/io/github/vshnv/nortviz/controller/ImageManagementController.java
@@ -0,0 +1,38 @@
+package io.github.vshnv.nortviz.controller;
+
+import io.github.vshnv.nortviz.entity.response.imgur.ImgurImageMeta;
+import io.github.vshnv.nortviz.service.ImgurService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+
+@Controller
+@RequestMapping("/api/v1")
+public class ImageManagementController {
+
+    private final ImgurService imgurService;
+    @Autowired
+    public ImageManagementController(ImgurService imgurService) {
+        this.imgurService = imgurService;
+    }
+
+    @PostMapping("/upload")
+    @Async
+    public @ResponseBody CompletableFuture<ResponseEntity<String>> uploadImage(@RequestPart("file") MultipartFile file) {
+        try {
+            final byte[] data = file.getBytes();
+            return imgurService.uploadImage(data, file.getName())
+                    .thenApply((meta) -> ResponseEntity.ok(meta.getId()))
+                    .exceptionally((exception) -> ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build());
+        } catch (IOException exception) {
+            return CompletableFuture.completedFuture(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build());
+        }
+    }
+}

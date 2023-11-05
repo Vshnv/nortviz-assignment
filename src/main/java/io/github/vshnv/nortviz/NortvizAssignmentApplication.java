09014f9ac1d327416ad1a9b47e86d1e667d9bfa5
Vaishnav Anil <vaishnav.nair@horizonind.org>
2023-11-05 17:29:21 +0530

    Initial commit

---
+++ src/main/java/io/github/vshnv/nortviz/NortvizAssignmentApplication.java
@@ -0,0 +1,13 @@
+package io.github.vshnv.nortviz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NortvizAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NortvizAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/vshnv/nortviz/NortvizAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vshnv.nortviz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NortvizAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

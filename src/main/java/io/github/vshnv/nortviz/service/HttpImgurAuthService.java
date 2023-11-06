8553b7d5242d8d9a87480c5aad88964add52b564
Vaishnav Anil <vaishnav.nair@horizonind.org>
2023-11-06 14:22:23 +0530

    Added comments for ImgurAuthService

--- src/main/java/io/github/vshnv/nortviz/service/HttpImgurAuthService.java
+++ src/main/java/io/github/vshnv/nortviz/service/HttpImgurAuthService.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * {@inheritDoc}
+ */
 @Service
 public class HttpImgurAuthService implements ImgurAuthService{
     private static final String BASE_URL = "https://api.imgur.com";
--- src/main/java/io/github/vshnv/nortviz/service/ImgurAuthService.java
+++ src/main/java/io/github/vshnv/nortviz/service/ImgurAuthService.java
@@ -4,6 +4,13 @@ import io.github.vshnv.nortviz.entity.response.imgur.ImgurAuthRefreshResponse;
 
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * Handles authentication for Imgur
+ */
 public interface ImgurAuthService {
+    /**
+     * Renews and fetches auth token from Imgur
+     * @return Auth token response
+     */
     ImgurAuthRefreshResponse authenticate();
 }
